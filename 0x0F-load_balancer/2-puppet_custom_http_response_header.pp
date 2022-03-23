e8d8824a35f494176c8117cea0ddac5d82fe1023
Coker Oluwasegun <cokeroluwasegun@outlook.com>
2022-03-22 17:52:06 -0700

    Created 2-puppet_custom_http_response_header.pp

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,18 @@
+# Puppet that configures nginx custom header
+
+exec { 'update_packages':
+  path    => ['/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/'],
+  command => 'sudo apt-get -y update',
+}
+
+exec { 'start_nginx':
+  path     => ['/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/'],
+  command  => 'sudo apt-get -y install nginx',
+  provider => 'shell',
+}
+
+exec { 'restart_nginx':
+  path     => ['/bin/', '/sbin/', '/usr/bin', '/usr/sbin'],
+  command  => 'sed -i -e "/sendfile/i \\\tadd_header X-Served-By $HOSTNAME;" /etc/nginx/nginx.conf && sudo service nginx restart',
+  provider => 'shell',
+}
