e1d22df592cce49529e359ca8d31d803dc205933
Oluwasegun Coker <cokeroluwasegun@outlook.com>
2022-04-11 20:35:40 +0100

    Created 0-gather_data_from_an_API.py file

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,26 +1,32 @@
 #!/usr/bin/python3
-""" Script that gathers to do data from an api """
+"""
+Script that gathers to do data from an api
+"""
 
-
-if __name__=="__main__":
+if __name__ == '__main__':
     import requests
-    from sys import argv
+    import sys
 
-    link = "https://jsonplaceholder.typicode.com/users"
-    uid = argv[1]
-    request_info = requests.get("{}/{}".format(link, uid)).json()
-    if request_info.status_code == 200:
-        emp_name = request_info['name']
-        todo_content = requests.get("{}/{}/todos".format(link, uid)).json()
-        done = list()
-        for todo in todo_content:
-            if todo['completed'] is True:
-                done.append(todo['title'])
-        total = len(todo_content)
-        total_done = len(done)
-        string = "Employee {} is done with task({}/{}):".format(emp_name,
-                                                            total_done,
-                                                            total)
-        print(string)
-        for task in done:
-            print("\t", task)
+    if len(sys.argv) < 2:
+        print('Usage: {} EMPLOYEE_ID'.format(argv[0]))
+        exit()
+
+    user_id = sys.argv[1]
+    url = 'https://jsonplaceholder.typicode.com/users/'
+    req = requests.get("{}/{}".format(url, user_id), verify=False)
+    if req.status_code == 200:
+        content = req.json()
+        employee_name = content.get('name')
+        total_tasks = 0
+        done_tasks = []
+        done_tasks_number = 0
+        to_do = requests.get(url + '{}/todos'.format(user_id)).json()
+        total_tasks = len(to_do)
+        for task in to_do:
+            if task.get('completed', None):
+                done_tasks.append(task.get('title'))
+        done_tasks_number = len(done_tasks)
+        print('Employee {} is done with tasks({}/{}):'.format(
+                employee_name, done_tasks_number, total_tasks))
+        for task in done_tasks:
+            print('\t {}'.format(task))
