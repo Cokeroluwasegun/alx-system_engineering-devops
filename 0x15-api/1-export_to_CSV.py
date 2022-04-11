b6d124cfb308a53a69aa1af35bb429df9f124fa0
Oluwasegun Coker <cokeroluwasegun@outlook.com>
2022-04-11 20:59:11 +0100

    Created 1-export_to_CSV.py

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Python Script """
+import csv
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    u_link = "https://jsonplaceholder.typicode.com/users"
+    uid = argv[1]
+
+    u_info = requests.get("{}/{}".format(u_link, uid)).json()
+    username = u_info['username']
+    todo_info = requests.get("{}/{}/todos".format(u_link, uid)).json()
+    file = '{}.csv'.format(uid)
+    with open(file, 'w') as csvfile:
+        writer = csv.writer(csvfile, delimiter=',',
+                            quoting=csv.QUOTE_ALL)
+        for todo in todo_info:
+            status = todo['completed']
+            title = todo['title']
+            writer.writerow([uid, username, status, title])
