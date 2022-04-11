79408add3478fb612b51c9c6d253323c3fe536e0
Oluwasegun Coker <cokeroluwasegun@outlook.com>
2022-04-11 21:06:53 +0100

    Created 2-export_to_JSON.py file

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,27 +1,32 @@
 #!/usr/bin/python3
-""" Python Script """
-import json
-import requests
-from sys import argv
+"""
+Script that gathers to do data from an api and exports it to a JSON file
+"""
 
-if __name__ == "__main__":
-    u_link = "https://jsonplaceholder.typicode.com/users"
-    uid = argv[1]
+if __name__ == '__main__':
 
-    u_info = requests.get("{}/{}".format(u_link, uid)).json()
-    username = u_info['username']
-    todo_info = requests.get("{}/{}/todos".format(u_link, uid)).json()
-    file = '{}.json'.format(uid)
-    data = dict()
-    data[str(uid)] = []
-    for todo in todo_info:
-        data[str(uid)].append(
-            {
-                "task": todo['title'],
-                "completed": todo['completed'],
-                "username": username
-            }
-        )
+    import json
+    import requests
+    import sys
 
-    with open(file, 'w', newline='') as jsonfile:
-        json.dump(data, jsonfile)
+    if len(sys.argv) < 2:
+        print('Usage: {} EMPLOYEE_ID'.format(argv[0]))
+        exit()
+    user_id = sys.argv[1]
+    file_name = '{}.json'.format(user_id)
+    url = 'https://jsonplaceholder.typicode.com/users/'
+    response = requests.get("{}/{}".format(url, user_id), verify=False)
+    if response.status_code == 200:
+        content = response.json()
+        to_do = requests.get(url + '{}/todos'.format(user_id)).json()
+        output = {}
+        output[user_id] = []
+        for row in to_do:
+            data = {}
+            data["task"] = row.get('title')
+            data["completed"] = row.get('completed')
+            data["username"] = content.get('username')
+            output[user_id].append(data)
+
+        with open(file_name, 'w', encoding='utf-8') as jf:
+            json.dump(output, jf)
